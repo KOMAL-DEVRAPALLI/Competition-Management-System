eb12be6464ded5a2cf976888b7c3f0f585e8a687
Komal Devrapalli <komaldevlapeeli@gmail.com>
2026-07-25 21:03:34 +0530

    Fix frontend

--- Frontend/api/axios.js
+++ Frontend/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const apiRequest = async (url, method, body = null) => {
     const res = await axios({
