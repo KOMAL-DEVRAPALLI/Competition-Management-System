2c6137e34ee97a30f2dca161d816838181781d99
Komal Devrapalli <komaldevlapeeli@gmail.com>
2026-07-26 01:00:15 +0530

    Add SMTP verification logging

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,14 @@ dotenv.config();
 
 const app = express();
 connectDB()
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://sdwa-registration-portal.vercel.app"
+    ],
+    credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/api", publicRoutes);
